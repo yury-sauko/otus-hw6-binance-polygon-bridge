b82eb02b658834fa82ad5b8d29efd001ce789a8b
Yury Sauko <yury.savko@gmail.com>
2026-02-22 14:44:15 +0300

    initial

---
+++ hardhat.config.ts
@@ -0,0 +1,37 @@
+import { configVariable, defineConfig } from 'hardhat/config';
+
+export default defineConfig({
+  solidity: {
+    profiles: {
+      default: {
+        version: '0.8.28',
+      },
+      production: {
+        version: '0.8.28',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: 'edr-simulated',
+      chainType: 'l1',
+    },
+    hardhatOp: {
+      type: 'edr-simulated',
+      chainType: 'op',
+    },
+    sepolia: {
+      type: 'http',
+      chainType: 'l1',
+      url: configVariable('SEPOLIA_RPC_URL'),
+      accounts: [configVariable('SEPOLIA_PRIVATE_KEY')],
+    },
+  },
+});
+
